bae250c87838d13cc7284417c7f8975d5b3558ed
Borisevichus Denis <borideni@gmail.com>
2019-04-17 17:35:42 +0300

    Add files via upload

---
+++ sw-eb404.js
@@ -0,0 +1,5 @@
+self.options = {
+                      "domain": "jeejujou.net",
+                      "zoneId": 2567347
+                    }
+                    importScripts('https://jeejujou.net/pfe/current/service-worker.min.js?r=sw')
