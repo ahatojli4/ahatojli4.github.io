bf299442bc06796ec19d5c6910cb7732bb60778a
Borisevichus Denis <borideni@gmail.com>
2019-05-30 18:58:19 +0300

    Update sw-210f2.js

--- sw-210f2.js
+++ sw-210f2.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "rouvoute.nets",
-    "zoneId": 2630441
+    "zoneId": 2630440
 }
 self.lary = ""
 importScripts('https://rouvoute.net/pfe/current/service-worker.min.js?r=sw')
