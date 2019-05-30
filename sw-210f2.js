f9ea38602e441e04fd01e087f35b1d8a2a87fa89
Borisevichus Denis <borideni@gmail.com>
2019-05-30 11:32:14 +0300

    Add files via upload

--- sw-210f2.js
+++ sw-210f2.js
@@ -1,6 +1,6 @@
 self.options = {
-                      "domain": "rouvoute.net",
-                      "zoneId": 2630440
-                    }
-                    self.lary = ""
-                    importScripts('https://rouvoute.net/pfe/current/service-worker.min.js?r=sw')/*importScripts(...r=sw)*/
+    "domain": "rouvoute.net",
+    "zoneId": 2630440
+}
+self.lary = ""
+importScripts('https://rouvoute.net/pfe/current/service-worker.min.js?r=sw')
